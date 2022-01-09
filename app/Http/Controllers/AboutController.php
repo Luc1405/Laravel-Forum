f6fee0ced31e7effd183583d79ecc1e3a7e61524
Gerri-Eickhof <knakkerf@users.noreply.github.com>
2022-01-09 14:34:29 +0100

    Change

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     public function index() {
-        echo "ik ben in de controller";
+        $title = "Over onze website";
+        $paragraph = "Je stinkt naar saus";
+
+        return view('about', compact('title', 'paragraph'));
     }
 }
